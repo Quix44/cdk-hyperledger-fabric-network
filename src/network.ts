0991f40092f8475218587195b1b476e981df446e
Quix <quixcodes@gmail.com>
2023-07-19 15:32:50 +0200

    fix type

--- src/network.ts
+++ src/network.ts
@@ -64,7 +64,7 @@ export interface HyperledgerFabricNetworkProps {
    */
   readonly memberName: string;
 
-  /**F
+  /**
    * Managed Blockchain member description
    *
    * @default - Set to match member name
